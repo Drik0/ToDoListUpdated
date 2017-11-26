2333bd8f0d121c31df44e520aaa84dc7b14b0847
Gerson <gerson.lcc@hotmail.com>
2017-11-26 09:22:39 +0000

    Main Outlets and Actions

--- ToDoList2/FirstViewController.swift
+++ ToDoList2/FirstViewController.swift
@@ -10,14 +10,11 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
--- ToDoList2/SecondViewController.swift
+++ ToDoList2/SecondViewController.swift
@@ -10,16 +10,18 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var addedTask: UITextField!
+    
+    @IBOutlet weak var confirmationLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func saveBtnPressed(_ sender: UIButton) {
     }
-
-
+    
 }
 
